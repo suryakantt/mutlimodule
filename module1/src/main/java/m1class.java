90fbe19adea217922e0d09fbc5d3208f8946d8d4
suryakantt <ssuryakant.sharmaa@gmail.com>
2021-10-03 10:33:22 +0530

    first commit

---
+++ module1/src/main/java/m1class.java
@@ -0,0 +1,5 @@
+public class m1class {
+    public static void main(String[] args) {
+        System.out.println("module1");
+    }
+}
---
+++ module2/src/main/java/m2class.java
@@ -0,0 +1,7 @@
+public class m2class {
+
+        public static void main(String[] args) {
+            System.out.println("module2");
+        }
+}
+
